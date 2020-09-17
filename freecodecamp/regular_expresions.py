f259c27456e45921c874a0bb8b4a99de171aafd9
Spike Storm <software.j2ee@gmail.com>
2020-09-17 03:34:53 -0700

    Directory changes for MATH course

---
+++ freecodecamp/regular_expresions.py
@@ -0,0 +1,14 @@
+import re
+
+
+if __name__ == "__main__":
+    # ^X.* All the words that start with an X followed by any character
+    with open("texts/elon_tweets.txt", mode="r") as handler_f:
+        for line in handler_f:
+            line = line.strip().lower()
+            if re.search(r"falcon", line):
+                print(line)
+
+    s = 'A message from csev@umich.edu to cwen@iupui.edu about meeting @2PM'
+    lst = re.findall('\\S+@\\S+', s)
+    print(lst)
