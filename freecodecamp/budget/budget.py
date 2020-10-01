90929d3f26831608e0d21daa9fedbcfc2a31d06f
Spike Storm <software.j2ee@gmail.com>
2020-09-30 19:47:48 -0700

    budget deposit methods

--- freecodecamp/budget/budget.py
+++ freecodecamp/budget/budget.py
@@ -21,9 +21,64 @@ class Category:
         self.category_name = category_name
         self.ledger = list()
 
-    def deposit(self, amount, description):
-        # {"amount": amount, "description": description}
+    def deposit(self, amount, description=""):
+        """
+        A deposit method that accepts an amount and description. If no description is given, it should default to an
+        empty string. The method should append an object to the ledger list in the form of
+        {"amount": amount, "description": description}.
+        :param amount:
+        :param description:
+        :return:
+        """
         self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description):
+        """
+        A withdraw method that is similar to the deposit method, but the amount passed in should be stored in the ledger
+        as a negative number. If there are not enough funds, nothing should be added to the ledger. This method should
+        return True if the withdrawal took place, and False otherwise.
+        :param amount:
+        :param description:
+        :return:
+        """
+        total = self.get_total_deposit()
+        if amount < total:
+            self.ledger.append({"amount": -1 * amount, "description": description})
+            return True
+        return False
+
+    def get_total_deposit(self):
+        total = 0
+        for transaction in self.ledger:
+            if transaction["amount"] > 0:
+                total = total + transaction["amount"]
+        return total
+
+    def get_balance(self):
+        """
+        A get_balance method that returns the current balance of the budget category based on the deposits
+        and withdrawals that have occurred.
+        :return:
+        """
+        balance = 0
+        for transaction in self.ledger:
+            balance = balance + transaction.amount
+        return balance
+
+    def transfer(self, amount, category):
+        """
+        A transfer method that accepts an amount and another budget category as arguments. The method should add a
+        withdrawal with the amount and the description "Transfer to [Destination Budget Category]". The method should
+        then add a deposit to the other budget category with the amount and the description "Transfer from [Source
+        Budget Category]". If there are not enough funds, nothing should be added to either ledgers. This method
+        should return True if the transfer took place, and False otherwise. :return:
+        """
+        are_funds = self.withdraw(amount, f"Transfer to {self.category_name}")
+        if are_funds:
+            category.deposit(amount, f"Transfer from {category.category_name}")
+            return True
+        else:
+            return False
 
     def __str__(self):
         len_cat_name = len(self.category_name)
@@ -38,8 +93,10 @@ class Category:
 
 
 if __name__ == "__main__":
-    c = Category("Food")
-    c.deposit(123.34, "Food Deposit1234567890")
-    c.deposit(3.34, "More food plant based")
-    c.deposit(55.34, "Plant Based food 12345678")
-    print(c)
+    food = Category("Food")
+    entretenimiento = Category("Entertainment")
+    business = Category("Business")
+    food.deposit(5000, "Deposito para comidas")
+    entretenimiento.deposit(3000, "Dinerito para entretenimiento")
+    business.deposit(2500.55, "Deposito para hacer negocios")
+
