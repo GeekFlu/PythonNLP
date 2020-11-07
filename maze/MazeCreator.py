bc50b679be1e22ab29216393b993525afbc4e898
Spike Storm <software.j2ee@gmail.com>
2020-11-07 10:00:49 -0800

    Shape calculator with OOP

--- maze/MazeCreator.py
+++ maze/MazeCreator.py
@@ -1,23 +1,15 @@
-from tkinter import *
+import time
 
 
-class Window(Frame):
+class Window:
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
-        self.init_window()
+        pass
 
     def init_window(self):
-        self.master.title("Maze Creator Ver 1.0")
-        self.pack(fill=BOTH, expand=1)
-        quitBtn = Button(self, text="Quit", fg="red", command=quit)
-        quitBtn.place(x=0, y=0)
+        pass
 
 
 # For installing tkinter on Ubuntu run sudo apt install python3-tk
 if __name__ == "__main__":
-    root = Tk()
-    root.geometry("400x300")
-    app = Window(root)
-    root.mainloop()
+    print(f'Welcome home Maze creator {time.time()}')
